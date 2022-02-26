5fae88bf285fafba4dcd5cf2a1f03a44b13d93de
Chloe A <ama1049@nyu.edu>
2022-02-25 22:52:04 -0500

    added test.yml

---
+++ tests/test_mealplan.py
@@ -0,0 +1,11 @@
+import ../src/webapp/mealplan
+import json
+
+def test_gen_meal_plan():
+    #TODO: health reqs
+    result_json = mealplan.gen_meal_plan("")
+    assert(type(result_json) == str)
+    result = json.loads(result_json)
+    assert(type(result) == list)
+    for i in result:
+        assert(type(i) == dict)
