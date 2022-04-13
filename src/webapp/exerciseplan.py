b1052df4b2704bba660e6877a614ddc712ba380e
Chloe A <ama1049@nyu.edu>
2022-04-13 15:26:22 -0400

    Added documentation to functions
    Weekly exercise plan generation algorithm description:
            For each day of the week that the user wants to exercise,
            generate a daily exercise plan
    
    Daily exercise plan generation algorithm description:
            Loop through all known exercises. For each exercise:
                    IF it targets a muscle that the user wants to work AND
                        its level is no higher than the user's level,
                            ADD it to the list
                    ELSE continue
                    Keep going until:
                            The exercise plan's total level is 3x the user's
                                level OR
                            A full loop has been completed with no new
                                exercises (i.e. no more valid exercises exist)

--- src/webapp/exerciseplan.py
+++ src/webapp/exerciseplan.py
@@ -16,12 +16,15 @@ def get_exercises_from_db():
             exercises.append(line)
     return exercises
 
-
-
 class ExerciseplanGenerator(data_src.DataStructures):
     user_requirements = None
     exercise_list = [] 
     def __init__(self, json_str):
+        """
+        Init function
+        in: JSON string formatted according to DataStructures.exercise_reqs
+        out: n/a
+        """
         self.user_requirements = json.loads(json_str)
         json_exercises = get_exercises_from_db()
         for i in json_exercises:
@@ -33,7 +36,9 @@ class ExerciseplanGenerator(data_src.DataStructures):
     #every day they do at least one exercise that targets every muscle group specified
     def _overlap(self, needed_groups, exercise):
         """
-        Check whether an exercise overlaps with the user's target muscle groups
+        Check whether an exercise overlaps with the target muscle groups
+        Returns True if needed_groups and exercise["targetmusclegroups"] have an overlapping member
+        Returns False otherwise.
         """
         for i in needed_groups:
             if i in exercise["targetmusclegroups"]:
@@ -43,6 +48,22 @@ class ExerciseplanGenerator(data_src.DataStructures):
     def _gen_exercises_for_day(self):
         """
         Returns a list of exercises for one day that satisfy the user's requirements
+
+        in: N/A
+        out: List of exercises that satisfy user requirements
+
+        Basic algorithm:
+            Shuffle exercise list
+            Find exercises that
+                overlap with at least one of the user's target muscle groups and
+                have a level no higher than the user's level
+            Add the exercise level to a running counter
+            Once the counter hits 3x the user's level the plan is returned.
+
+            It is also programmed to try to find a plan that satisfies *all* of the user's requirements and to avoid repeats.
+            Tweaks can be done to allow repeats or non-overlapping exercises if there are no more good exercises,
+            or the entire generator could be rebuilt to rank each exercise and pick the top N. But I think this returns
+            reasonable exercise plans given the user's requirements.
         """
         user_exercises = []
         exercise_sum = 0
@@ -74,8 +95,14 @@ class ExerciseplanGenerator(data_src.DataStructures):
         return user_exercises
 
     def gen_exercise_plan(self):
+        """
+        Returns a weekly exercise plan populated with exercises for every day that the user wants to work out.
+
+        in: N/A
+        out: Weekly exercise plan in json format
+        """
         to_return = DataStructures.exercise_plan()
         for i in self.user_requirements["days"]:
             if self.user_requirements["days"][i]:
                 to_return[i] = self._gen_exercises_for_day()
-        return to_return
+        return json.dumps(to_return)
