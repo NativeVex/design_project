8203f382ce2c54184e864c4b92994c6164033d32
Aj <commonoxygen@gmail.com>
2022-04-23 17:32:50 -0400

    UC#2_E2E#1 completed #2cwutev[In Review]

--- tests/UC_1_login_to_existing_account/UI_component/test_login.py
+++ tests/UC_1_login_to_existing_account/UI_component/test_login.py
@@ -8,15 +8,3 @@ def test_login_page(test_client):
     assert response.status_code == 200
     assert b"Login to your Health/Diet Planner Account" in response.data
 
-
-def test_login_failed(test_client):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/' page is requested (POST)
-    THEN check that the response is valid
-    """
-    response = test_client.post(
-        "/", data=dict(email="any@gmail.com", password="notsome"), follow_redirects=True
-    )
-    assert response.status_code == 200
-    assert b"Please check your login details and try again." in response.data
